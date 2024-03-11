d049b11b9e03b4c081b085e66808ea2f48aef79f
night-runner-dh <161902344+night-runner-dh@users.noreply.github.com>
2024-03-11 02:17:19 +0000

    第5章finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/edit'
 
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   get '/top' => 'homes#top'
   
    # .../lists/1 や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show'
+  get 'lists/:id' => 'lists#show', as: 'list'
+  
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+    patch 'lists/:id' => 'lists#update', as: 'update_list'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
